9a0010cedfe280fc21f8fdf0392bd321e1026db6
kenya0411 <shimoda.kenya@gmail.com>
2022-02-25 18:10:11 +0900

    test

--- laravel/database/migrations/2022_02_25_051536_create_customers_table.php
+++ laravel/database/migrations/2022_02_25_051536_create_customers_table.php
@@ -16,40 +16,17 @@ class CreateCustomersTable extends Migration
         Schema::create('customers', function (Blueprint $table) {
             // $table->bigIncrements('id');
             // $table->timestamps();
-            $table->integer('id', true);
+            $table->integer('customers_id', true);
             $table->string('customers_product_id')->nullable();
-            $table->string('customers_id')->nullable();
             $table->string('customers_nickname')->nullable();
             $table->string('customers_name')->nullable();
-            $table->integer('date_year')->nullable();
-            $table->integer('date_month')->nullable();
-            $table->integer('date_day')->nullable();
-            $table->integer('persons_id')->nullable();
-            $table->integer('products_id')->nullable();
-            $table->integer('products_options_id')->nullable();
-            $table->integer('customers_etc_price')->nullable();
+            $table->string('customers_name')->nullable();
             $table->text('customers_address')->nullable();
             $table->text('customers_note')->nullable();
             $table->string('customers_age')->nullable();
             $table->string('customers_prefecture')->nullable();
-            $table->text('customers_worry')->nullable();
-            $table->text('customers_answer')->nullable();
-            $table->text('customers_reply1')->nullable();
-            $table->text('customers_reply_answer1')->nullable();
-            $table->text('customers_reply2')->nullable();
-            $table->text('customers_reply_answer2')->nullable();
-            $table->text('customers_reply3')->nullable();
-            $table->text('customers_reply_answer3')->nullable();
-            $table->text('customers_deliver_notice')->nullable();
-            $table->string('customers_deliver_add_product')->nullable();
             $table->timestamps('created_at')->nullable();
             $table->timestamps('updated_at')->nullable();
-            $table->string('customers_fortune_is_finished')->nullable();
-            $table->string('customers_ship_is_finished')->nullable();
-            $table->integer('users_id')->nullable();
-            $table->string('product_name')->nullable();
-            $table->string('products_options_name')->nullable();
-            $table->string('persons_name')->nullable();
         });
     }
 
--- laravel/database/migrations/2022_02_25_055443_create_orders_table.php
+++ laravel/database/migrations/2022_02_25_055443_create_orders_table.php
@@ -14,8 +14,33 @@ class CreateOrdersTable extends Migration
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+            
+            $table->integer('id', true);
+            $table->string('orders_id')->nullable();
+            $table->integer('customers_id')->nullable();
+            $table->string('products_id')->nullable();
+            $table->string('product_options_id')->nullable();
+            $table->integer('users_id')->nullable();
+            $table->integer('orders_price')->nullable();
+            $table->integer('orders_etc_price')->nullable();
+            $table->text('orders_worry')->nullable();
+            $table->text('orders_answer')->nullable();
+            $table->text('orders_reply1')->nullable();
+            $table->text('orders_reply_answer1')->nullable();
+            $table->text('orders_reply2')->nullable();
+            $table->text('orders_reply_answer2')->nullable();
+            $table->text('orders_reply3')->nullable();
+            $table->text('orders_reply_answer3')->nullable();
+            $table->text('orders_deliver_notice')->nullable();
+            $table->string('orders_deliver_add_product')->nullable();
+            $table->timestamps('created_at')->nullable();
+            $table->timestamps('updated_at')->nullable();
+            $table->string('customers_fortune_is_finished')->nullable();
+            $table->string('customers_ship_is_finished')->nullable();
+            $table->integer('users_id')->nullable();
+            $table->string('product_name')->nullable();
+            $table->string('products_options_name')->nullable();
+            $table->string('persons_name')->nullable();
         });
     }
 
