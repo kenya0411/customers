38438534a325d69277892d04b61a976afa08c137
kenya0411 <shimoda.kenya@gmail.com>
2022-03-17 10:37:48 +0900

    テスト用

---
+++ laravel/resources/views/orders/components/test.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="teleport-component">
+    <h3>Teleport元（コンポーネント）</h3>
+    <div>↓↓↓↓Teleport元↓↓↓↓</div>
+    <teleport to="#teleport-target"> <!--（1）-->
+      このテキストがTeleportされます
+    </teleport>
+    <div>↑↑↑↑Teleport元↑↑↑↑</div>
+  </div>
+</template>
