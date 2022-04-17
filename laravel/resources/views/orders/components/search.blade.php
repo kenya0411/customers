36c31a9c546253c8c2e9bebfe500a497cc0ee65d
kenya0411 <shimoda.kenya@gmail.com>
2022-04-17 21:49:43 +0900

    css修正

--- laravel/resources/views/orders/components/search.blade.php
+++ laravel/resources/views/orders/components/search.blade.php
@@ -3,14 +3,14 @@
 
 @section('search_content')
 
-<li class="me-3">
+<li class="me-3 wid100">
 <div class="input-group">
 <input type="text"  placeholder="商品ID" aria-label="商品ID" aria-describedby="input-group-left" name="search_text" v-model="search_orders_id">
 <span class="input-group-text" id="input-group-left-example"><i class="fa-solid fa-magnifying-glass"></i></span>
 
 </div>
 </li>
-<li class="me-3">
+<li class="me-3 wid100">
 <div class="input-group">
 <input type="text"  placeholder="顧客名" aria-label="顧客名" aria-describedby="input-group-left" name="search_name" v-model="search_customers_name">
 <span class="input-group-text" id="input-group-left-example"><i class="fa-solid fa-magnifying-glass"></i></span>
