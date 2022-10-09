5156c4cbb5ede64605bf23a10a5c133750947844
kenya0411 <shimoda.kenya@gmail.com>
2022-10-09 21:52:49 +0900

    コメント編集者用の表示・非表示

--- laravel/resources/views/common/components/side_list.blade.php
+++ laravel/resources/views/common/components/side_list.blade.php
@@ -94,7 +94,15 @@
    <li class="menu_list">
     <a href="/orders">注文管理</a>
   </li>
-
+   <li class="menu_list">
+    <a href="#">LINE管理</a>
+    <div class="menu_contents">
+      <ul>
+        <li><a href="/lines">LINEメッセージ</a></li>
+        <li><a href="/lines/mails">メール設定</a></li>
+      </ul>
+    </div>
+  </li>
 </ul>
 
 @endcan
