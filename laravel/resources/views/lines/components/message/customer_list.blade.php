09d5b4802f000fe7e185f0575d27300a534ac11f
kenya0411 <shimoda.kenya@gmail.com>
2023-07-05 01:50:36 +0900

    LINEメッセージのニューメッセージ返信

--- laravel/resources/views/lines/components/message/customer_list.blade.php
+++ laravel/resources/views/lines/components/message/customer_list.blade.php
@@ -5,11 +5,17 @@
  </div>
  <ul v-for="(line_list, index) in lines_customers">
  	<li>
-				<a v-bind:href='`/lines?userid=${line_list.lines_customers_userid}`' class="link">
+        <a v-bind:href='`/lines?userid=${line_list.lines_customers_userid}`' class="link">
+
+        <div class="arrow"></div>
+        <div v-if="line_list.lines_messages_to_userid!==line_list.lines_customers_userid && line_list.lines_messages_ngword!==true" class="new_message"><span>要返信</span></div>
+        <div class="link_name">
+            
  		@{{ line_list.lines_customers_name }}
+        </div>
 
  		</a>
-        <span v-if="line_list.lines_messages_to_userid!==line_list.lines_customers_userid && line_list.lines_messages_ngword!==true" class="new_message">New</span>
+        
  	</li>
  </ul>
  
